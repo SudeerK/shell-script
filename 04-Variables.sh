b72764a567adcaff2d5738c2f3c8e55c34319471
SUDEER.K <sudeer.k@gmail.com>
2023-12-20 18:28:25 +0530

    Passing value to a variable in the script

---
+++ 04-Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hello $PERSON2, Good Morning. How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine"
+echo "$PERSON1: Let us meet and discuss once"
+echo "$PERSON2: Sure, Fix some date and let me know"
