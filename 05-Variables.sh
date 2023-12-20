25042c17b434a042b05899c01808fb046d7bc97c
SUDEER.K <sudeer.k@gmail.com>
2023-12-20 19:59:57 +0530

    updates

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -2,7 +2,8 @@
 
 
 echo "Please enter your UserName:"
-read USERNAME
+# read USERNAME # This will take the input in runtime and pass in below commands.
+read -s USERNAME # This will take the input in runtime and will not show up.
 
 echo "USERNAME is $USERNAME"
 echo "PASSWORD is $PASSWORD"
