fdeadbbd27de7a0d4080b5e49987837899a2d0b9
SUDEER.K <sudeer.k@gmail.com>
2023-12-20 21:51:37 +0530

    updates

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -8,4 +8,4 @@ echo "Third fruit is : $(FRUITS(2))"
 
 echo "First fruit is : ${FRUITS[0]}"
 echo "Second fruit is : ${FRUITS[1]}"
-echo "Third fruit is : ${FRUITS[2]}"
+echo "Thirdd fruit is : ${FRUITS[2]}"
