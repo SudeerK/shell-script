b41b838dbf0dcffaa29a1ea8a61e4944cd402da9
SUDEER.K <sudeer.k@gmail.com>
2023-12-20 22:01:43 +0530

    updates

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -5,3 +5,5 @@ FRUITS=("APPLE" "BANANA" "Mango")
 echo "First fruit is : ${FRUITS[0]}"
 echo "Second fruit is : ${FRUITS[1]}"
 echo "Third fruit is : ${FRUITS[2]}"
+
+echo "All Fruits : ${FRUITS[@]}
