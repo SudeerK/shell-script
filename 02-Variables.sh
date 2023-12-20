18d0752b3c41e16a72c994d0f3dfbbadee00a87f
SUDEER.K <sudeer.k@gmail.com>
2023-12-20 17:09:19 +0530

    Modified with assigning variables

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -2,7 +2,7 @@
 
 PERSON1=RAMESH
 PERSON2=SURESH
-echo "$PERSON1: How are you?"
-echo "$PERSON2: I am fine"
+echo "$PERSON1: Hello $PERSON2, Good Morning. How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine"
 echo "$PERSON1: Let us meet and discuss once"
 echo "$PERSON2: Sure, Fix some date and let me know"
