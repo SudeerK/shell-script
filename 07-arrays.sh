910f6c0d224c747ed1e6c3a2fd50c2255cb62ed7
SUDEER.K <sudeer.k@gmail.com>
2023-12-20 21:47:04 +0530

    updates

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,6 +2,10 @@
 
 FRUITS=[APPLE,BANANA,Mango]
 
-echo "First fruit is : $FRUITS(0)"
-echo "First fruit is : $FRUITS(1)"
-echo "First fruit is : $FRUITS(2)"
+echo "First fruit is : $(FRUITS(0))"
+echo "First fruit is : $(FRUITS(1))"
+echo "First fruit is : $(FRUITS(2))"
+
+echo "First fruit is : $(FRUITS[0])"
+echo "First fruit is : $(FRUITS[1])"
+echo "First fruit is : $(FRUITS[2])"
