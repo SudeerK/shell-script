8d9a8609d72b041f8d93024b6e5103b1803afaf3
SUDEER.K <sudeer.k@gmail.com>
2023-12-21 07:59:03 +0530

    updates

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,9 +3,9 @@
 echo "Enter the Number:"
 read Number
 
-if [$Number -gt 100]
+if [ $Number -gt 100 ]
 then
-    echo "Number is greater than 100"
+    echo "Given Number $Number is greater than 100"
 else
-    echo "Number is less than 100"
+    echo "Given Number $Number is less than 100"
 fi
