3aa3b5fb90298a756b4eaeda73483c2aabfb599e
SUDEER.K <sudeer.k@gmail.com>
2023-12-21 11:41:33 +0530

    updates

--- 09-install-MySQL.sh
+++ 09-install-MySQL.sh
@@ -2,4 +2,11 @@
 
 ID=$(id -u)
 
-echo " The ID of the user is $ID"
+if [ $ID ne 0]
+echo " The user is not a root user. The ID of the user is $ID"
+
+else
+
+echo "The user is a root user. The ID of the user is $ID"
+
+fi
