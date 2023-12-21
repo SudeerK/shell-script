b531a3272dd417652c545d798d320f1b68def07e
SUDEER.K <sudeer.k@gmail.com>
2023-12-21 11:47:33 +0530

    updates

--- 09-install-MySQL.sh
+++ 09-install-MySQL.sh
@@ -2,14 +2,14 @@
 
 ID=$(id -u)
 
-if [ $ID -ne 0 ]
+if [ $ID -ne 0 ] #Spaces should be present before $ID and after 0
 
 then
 
-echo " The user is not a root user. The ID of the user is $ID"
+    echo " Error: The user is not a root user. The ID of the user is $ID. Please run the script with root user"
 
 else
 
-echo "The user is a root user. The ID of the user is $ID"
+    echo "The user is a root user. The ID of the user is $ID"
 
 fi
