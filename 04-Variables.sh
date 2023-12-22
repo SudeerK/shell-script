1c368f5d5400f9d3001083aca08d7d69b0249065
SUDEER.K <sudeer.k@gmail.com>
2023-12-22 15:24:04 +0530

    updates

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -7,5 +7,6 @@ echo "$PERSON2: Hi $PERSON1, I am fine"
 echo "$PERSON1: Let us meet and discuss once"
 echo "$PERSON2: Sure, Fix some date and let me know"
 
-echo "Total Number of args passed $#"
-echo " All args passed $@"
+echo "Total Number of args passed: $#"
+echo " All args passed: $@"
+echo "Script Name: $0"
