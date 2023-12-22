3f2d172e4c5022c56f8e6e08263d9878b12b6e28
SUDEER.K <sudeer.k@gmail.com>
2023-12-22 13:14:05 +0530

    updates

---
+++ 10-functions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE ()
+{
+    if [ $? -ne 0 ]
+    then
+        echo " ERROR: MySQL installation is failed"
+        exit 1
+    else
+        echo "MySQL installation is success"
+    fi
+}
+
+if [ $ID -ne 0 ] #Spaces should be present before $ID and after 0
+
+then
+
+    echo " Error: The user is not a root user. The ID of the user is $ID. Please run the script with root user"
+    exit 1 #Any Value other than 0, so that it will exit if this condition satisfies, else will continue. This is disadvantage in Shell
+
+else
+
+    echo "The user is a root user. The ID of the user is $ID"
+
+fi
+
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
