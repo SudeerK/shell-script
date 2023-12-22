fe670e68379544fae7216977e56fc281f5693b67
SUDEER.K <sudeer.k@gmail.com>
2023-12-22 15:23:13 +0530

    updates

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -6,3 +6,6 @@ echo "$PERSON1: Hello $PERSON2, Good Morning. How are you?"
 echo "$PERSON2: Hi $PERSON1, I am fine"
 echo "$PERSON1: Let us meet and discuss once"
 echo "$PERSON2: Sure, Fix some date and let me know"
+
+echo "Total Number of args passed $#"
+echo " All args passed $@"
--- 11-logs.sh
+++ 11-logs.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
+# $? - Exit Status of previous command
+# $0 - To retrieve the script name
+# $1, $2, ... $N - Args
+# $@ - All args
+# $# - Total Number of args
+
 
 echo " File Name is $0"
+
+ID=$(id -u)
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo " ERROR: $2 is failed"
+        exit 1
+    else
+        echo "$2 is success"
+    fi
+}
+
+if [ $ID -ne 0 ] #Spaces should be present before $ID and after 0
+
+then
+
+    echo " Error: The user is not a root user. The ID of the user is $ID. Please run the script with root user"
+    exit 1 #Any Value other than 0, so that it will exit if this condition satisfies, else will continue. This is disadvantage in Shell
+
+else
+
+    echo "The user is a root user. The ID of the user is $ID"
+
+fi
+
+
+yum install mysql -y
+
+VALIDATE $? "Installation of mySQL"
+
+yum install git -y
+
+VALIDATE $? "Installation of GIT"
